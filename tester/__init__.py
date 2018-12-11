2b979e4af1dd1ed99934f2eaa53f8f6c13beae06
Abhimanyu Pandian <36107381+abhimanyupandian@users.noreply.github.com>
2018-12-11 16:55:43 +0530

    Update __init__.py

--- tester/__init__.py
+++ tester/__init__.py
@@ -9,7 +9,7 @@ class BashScript(object):
 
     def __getattr__(self, name):
         def call_fun(*args):
-            script_path = "'{}'".format(os.path.dirname(os.path.realpath(self.script)))
+            script_path = "'{}'".format(os.path.realpath(self.script))
             return subprocess.check_output(['bash', '-c', 'source {} && {} {}'.format(script_path, name,
                                             " ".join([str(each) for each in args]))], universal_newlines=True)
 
